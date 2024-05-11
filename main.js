90ba039e267934daf4613a3a03cc780271291740
Abdelrahman Tayara <107851455+Tayara97@users.noreply.github.com>
2024-05-11 21:35:05 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+let shareIcon = document.querySelector(".share-icon");
+let appsShare = document.querySelector(".apps-sharing");
+shareIcon.addEventListener("click", () => appsShare.classList.toggle("open"));
